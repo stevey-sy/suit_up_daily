bfe8f5d6272575c80c37d82ae058f8629be076f1
stevey-sy <sinsy89@hanmail.net>
2020-12-10 19:42:38 +0900

    수정: 코디 상세 조회 class / 툴바 등록 (수정, 삭제버튼)

--- app/src/main/java/com/example/suitupdaily/CodiDetail.java
+++ app/src/main/java/com/example/suitupdaily/CodiDetail.java
@@ -1,11 +1,15 @@
 package com.example.suitupdaily;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,22 +19,35 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 
+import org.w3c.dom.Text;
+
 public class CodiDetail extends AppCompatActivity {
 
-    private String user_id, picture, tags, memo, idx, season;
+    private String user_id, picture, tags, memo, idx, season, date;
     private ImageView img_codi;
-    private TextView text_view_codi_season, text_view_codi_tags, text_view_codi_memo, input_tag;
+    private TextView text_view_codi_season, text_view_codi_tags, text_view_codi_memo, text_view_date;
     private Button button_remove_tag, button_input_tag;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private Menu action;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_codi_detail);
 
+        // 툴바 설정
+        toolbar = findViewById(R.id.toolbar_codi_detail);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+
         // xml 요소 연결
         img_codi = (ImageView) findViewById(R.id.image_view_codi_detail);
         text_view_codi_tags = (TextView) findViewById(R.id.text_view_tag_list);
         text_view_codi_memo = (TextView) findViewById(R.id.text_view_memo_detail);
+        text_view_date = (TextView) findViewById(R.id.text_view_date_detail);
 
         // ShowRoom 클래스에서 받은 intent 데이터
         Intent getCodiInfo = getIntent();
@@ -39,6 +56,7 @@ public class CodiDetail extends AppCompatActivity {
         season = getCodiInfo.getStringExtra("season");
         tags = getCodiInfo.getStringExtra("tags");
         memo = getCodiInfo.getStringExtra("memo");
+        date = getCodiInfo.getStringExtra("date");
 
         // 받아온 intent 데이터를 view 에 뿌려주는 메소드
         setDataFromIntentExtra();
@@ -48,9 +66,9 @@ public class CodiDetail extends AppCompatActivity {
     @SuppressLint("CheckResult")
     private void setDataFromIntentExtra() {
 
-        readMode();
         text_view_codi_tags.setText(tags);
         text_view_codi_memo.setText(memo);
+        text_view_date.setText("Date: " + date);
 
         RequestOptions requestOptions = new RequestOptions();
         requestOptions.skipMemoryCache(true);
@@ -64,10 +82,16 @@ public class CodiDetail extends AppCompatActivity {
                 .into(img_codi);
     }
 
-    // 사용자의 조회 모드
-    // 텍스트뷰를 선택해도 정보를 수정할 수 없다.
-    private void readMode() {
+    // 툴바 메뉴 불러오기
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.codi_detail_menu, menu);
 
+        action = menu;
 
+        return true;
     }
+
+
 }
