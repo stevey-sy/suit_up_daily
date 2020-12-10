f3ee9b6a6548f48f2dd36125367196afe3437768
stevey-sy <sinsy89@hanmail.net>
2020-12-10 15:10:51 +0900

    추가: 코디 상세정보 입력 class / 코디이미지+정보를 서버 업로드 기능

--- app/src/main/java/com/example/suitupdaily/CodiInfo.java
+++ app/src/main/java/com/example/suitupdaily/CodiInfo.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,33 +13,41 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.suitupdaily.dialog.DialogCodi;
 import com.example.suitupdaily.dialog.DialogCodiPlace;
 import com.example.suitupdaily.dialog.SeasonMultipleChoice;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static android.text.TextUtils.isEmpty;
 
 // 사용자가 코디 이미지를 선택 후, 코디에 대한 정보를 입력 할 수 있는 액티비티
 public class CodiInfo extends AppCompatActivity {
 
-    private ImageView image_view_codi;
     private Bitmap bitmap;
-    private TextView text_view_codi_season, text_view_codi_place;
-    private EditText text_input_tag;
-    private String string_hash_tag;
-    private Button button_tag_test, button_tag_remove;
+    private TextView text_view_codi_season, text_view_codi_place, tags_view;
+    private EditText text_input_tag, memo_area;
+    private String user_id, string_hash_tag;
+    private Button button_codi_upload;
 
     private ArrayList<String> mTagLists;
 
@@ -47,11 +56,14 @@ public class CodiInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_codi_info);
 
-        image_view_codi = (ImageView) findViewById(R.id.image_view_codi);
+        ImageView image_view_codi = (ImageView) findViewById(R.id.image_view_codi);
         text_view_codi_season = (TextView) findViewById(R.id.text_view_codi_season);
         text_input_tag = (EditText) findViewById(R.id.text_view_codi_tag);
-        button_tag_test = (Button) findViewById(R.id.button_tag_test);
-        button_tag_remove = (Button) findViewById(R.id.button_tag_remove);
+        Button button_tag_test = (Button) findViewById(R.id.button_tag_test);
+        Button button_tag_remove = (Button) findViewById(R.id.button_tag_remove);
+        button_codi_upload = (Button) findViewById(R.id.button_codi_upload);
+        tags_view = (TextView)findViewById(R.id.text_view_hash_tag);
+        memo_area = (EditText)findViewById(R.id.memo_area);
 //        text_view_codi_place = (TextView) findViewById(R.id.text_view_codi_place);
         // 해쉬 태그가 표시될 텍스트 뷰
 
@@ -61,6 +73,8 @@ public class CodiInfo extends AppCompatActivity {
         // SelfCodi 클래스에서 사용자가 편집한 이미지를 받아오는 intent
         Intent getDataFromSelfCodi = getIntent();
         byte[] arr = getDataFromSelfCodi.getByteArrayExtra("edited_image");
+        user_id = getDataFromSelfCodi.getStringExtra("userID");
+
 
         // intent 로 받아온 이미지(byte array)를 비트맵 으로 바꾸는 코드
         bitmap = BitmapFactory.decodeByteArray(arr, 0, arr.length);
@@ -147,14 +161,23 @@ public class CodiInfo extends AppCompatActivity {
         button_tag_remove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // 버튼을 누르면, 해쉬태그 리스트에서 가장 마지막 항목을 지우게 된다.
                 if (mTagLists.size() > 0) {
                     mTagLists.remove(mTagLists.size() -1);
                     setContent();
+
                 }
             }
         });
 
+        // 저장 버튼을 눌렀을 때의 이벤트
+        button_codi_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // retrofit으로 서버에 업로드하는 메소드
+                uploadCodi();
+            }
+        });
     }
 
     // 사용자가 입력한 문자열을 해쉬태그처럼 보이도록 하는 메서드
@@ -186,16 +209,17 @@ public class CodiInfo extends AppCompatActivity {
             tagsContent.setSpan(hashTag, hashTagStart, hashTagEnd, 0);
         }
 
-        TextView tags_view = (TextView)findViewById(R.id.text_view_hash_tag);
 //        tags_view.setVisibility(View.INVISIBLE);
 
         if(tags_view != null) {
             tags_view.setMovementMethod(LinkMovementMethod.getInstance());
             tags_view.setText(tagsContent);
             tags_view.setVisibility(View.VISIBLE);
+            // 해쉬태그 확인용 로그
+            Log.d("문자열 확인: ", tags_view.getText().toString());
         }
     }
-
+    // 해쉬태그 요소 순서용?
     public ArrayList<int[]> getSpans(String body, char prefix) {
         ArrayList<int[]> spans = new ArrayList<int[]>();
 
@@ -210,6 +234,71 @@ public class CodiInfo extends AppCompatActivity {
             spans.add(currentSpan);
         }
         return spans;
+    }
+
+    // 서버 업로드 메소드
+    private void uploadCodi() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("코디 데이터 추가 중...");
+        progressDialog.show();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 30, byteArrayOutputStream);
+
+        byte[] imageInByte = byteArrayOutputStream.toByteArray();
+
+        // 서버에 보낼 데이터 정의
+        String encodedImage = Base64.encodeToString(imageInByte, Base64.DEFAULT);
+        String id = user_id;
+        // 해쉬태그에서 "#" 문자만 제거
+        String hash_tag = tags_view.getText().toString();
+        String tags_no_hash = removeHash(hash_tag);
+        String memo = memo_area.getText().toString();
+
+        Call<ResponsePOJO> call = RetrofitClient.getInstance().getApi().uploadCodi(id, encodedImage, tags_no_hash, memo);
+        call.enqueue(new Callback<ResponsePOJO>() {
+            @Override
+            public void onResponse(Call<ResponsePOJO> call, Response<ResponsePOJO> response) {
+                progressDialog.dismiss();
+                Toast.makeText(CodiInfo.this, response.body().getRemarks(), Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(getApplicationContext(),MyClosetActivity.class);
+//                intent.putExtra("userID", user_id);
+//                startActivity(intent);//액티비티 띄우기
+
+                if(response.body().isStatus()) {
+
+                } else {
+
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponsePOJO> call, Throwable t) {
+                Toast.makeText(CodiInfo.this, "Network Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        Toast.makeText (this, encodedImage, Toast.LENGTH_SHORT).show();
+    }
+    // 해쉬태그 문자열에서 #만 제거하는 메소드
+    public String removeHash(String with_hash) {
+
+        // string 에서 특정 문자열 치환하는 코드
+//        with_hash = tags_view.getText().toString();
+        String no_hash;
+        no_hash = with_hash.replace("#", "");
+
+        Log.d("노해쉬: ", no_hash);
+
+//        // array list 에서 특정 문자열 제거하는 코드
+//        ArrayList<String> list = new ArrayList<String>(Arrays.asList("a", "b", "c", "d"));
+//        Iterator<String> iter = list.iterator();
+//        while (iter.hasNext()) {
+//            String s = iter.next();
+//            if (s.equals("a")) {
+//                iter.remove();
+//            }
+//        }
+        return no_hash;
     }
 
 }
