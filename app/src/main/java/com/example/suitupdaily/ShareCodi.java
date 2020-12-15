16cbf7029df9e017290fb20615f5fcfd9dca6d58
stevey-sy <sinsy89@hanmail.net>
2020-12-15 19:10:03 +0900

    수정: 코디 공유 페이지 / 리사이클러뷰 추가, 서버에서 데이터 받아오기 성공

--- app/src/main/java/com/example/suitupdaily/ShareCodi.java
+++ app/src/main/java/com/example/suitupdaily/ShareCodi.java
@@ -1,28 +1,90 @@
 package com.example.suitupdaily;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.suitupdaily.recycler.CodiShareAdapter;
+import com.example.suitupdaily.recycler.ShowRoomAdapter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ShareCodi extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private CodiShareAdapter adapter;
+    CodiShareAdapter.ShareViewClickListener listener;
+    private List<ResponsePOJO> clothList;
     private TextView filter_recently, filter_sex, filter_age;
+    private String user_id;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+
+    private TextView notify_no_codi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_codi);
 
+        //사용자 아이디를 받는 intent
+        Intent intent_id = getIntent();
+        user_id = intent_id.getStringExtra("userID");
+
+        // 툴바 설정
+        toolbar = findViewById(R.id.toolbar_share_codi);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+
         //xml 연결
         filter_recently = (TextView)findViewById(R.id.filter_recently);
         filter_sex = (TextView)findViewById(R.id.filter_sex);
         filter_age = (TextView)findViewById(R.id.filter_age);
+        notify_no_codi = (TextView)findViewById(R.id.notify_no_codi);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_codi_view);
 
-        // 필터 버튼 클릭 리스터
+        // 그리드 뷰의 한 열에 아이템의 갯수
+        int numberOfColumns =2;
+        // 리사이클러 뷰를 그리드 뷰 형식으로 사용하기 위해 선언
+        layoutManager = new GridLayoutManager(this, numberOfColumns);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // 리사이클러 아이템 클릭 시, 이벤트
+        listener = new CodiShareAdapter.ShareViewClickListener() {
+            @Override
+            public void onRowClick(View view, int position) {
+                // 다음 액티비티에 필요한 데이터를 intent에 담는다.
+//                Intent intent = new Intent(ShareCodi.this, CodiDetail.class);
+//                intent.putExtra("userID", user_id);
+//                intent.putExtra("idx", clothList.get(position).getIdx());
+//                intent.putExtra("picture", clothList.get(position).getPicture());
+//                intent.putExtra("season", clothList.get(position).getSeason());
+//                intent.putExtra("tags", clothList.get(position).getTags());
+//                intent.putExtra("memo", clothList.get(position).getMemo());
+//                intent.putExtra("date", clothList.get(position).getDate());
+//                startActivity(intent);
+            }
+        };
+
+        // 필터 버튼 클릭 리스너
         filter_recently.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,7 +99,6 @@ public class ShareCodi extends AppCompatActivity {
         PopupMenu popup = new PopupMenu (this, button);
         // xml 과 연결
         popup.getMenuInflater().inflate(R.menu.menu_filter_recently, popup.getMenu());
-
         // popup 메뉴 클릭 시 이벤트
         popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
@@ -52,5 +113,49 @@ public class ShareCodi extends AppCompatActivity {
             }
         });
         popup.show();
+    }
+
+    // 서버에서 코디 정보 가져오는 메서드
+    public void getCodiBoard() {
+
+        String id = user_id;
+
+        Call<List<ResponsePOJO>> call = RetrofitClient.getInstance().getApi().getCodiBoard(id);
+        call.enqueue(new Callback<List<ResponsePOJO>>() {
+            @Override
+            public void onResponse(Call<List<ResponsePOJO>> call, Response<List<ResponsePOJO>> response) {
+                clothList = response.body();
+                String content = "";
+
+                if(response.body() != null) {
+                    Log.d("서버 응답 확인: ", response.body().toString());
+                    Toast.makeText(getApplicationContext(), "Test 중입니다. 서버 통신 중" , Toast.LENGTH_SHORT).show();
+
+                    recyclerView.setVisibility(View.VISIBLE);
+                    notify_no_codi.setVisibility(View.GONE);
+
+                    adapter = new CodiShareAdapter(clothList, ShareCodi.this, listener);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                } else if (response.body() == null) {
+                    Toast.makeText(getApplicationContext(), "Test 중입니다. 서버 통신 실패" , Toast.LENGTH_SHORT).show();
+                    recyclerView.setVisibility(View.GONE);
+                    notify_no_codi.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<ResponsePOJO>> call, Throwable t) {
+                Toast.makeText(ShareCodi.this, "rp :"+
+                                t.getMessage().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getCodiBoard();
     }
 }
