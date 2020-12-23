13527facb594e5b46490c9505d09cd24b69f2d19
stevey-sy <sinsy89@hanmail.net>
2020-12-23 14:01:24 +0900

    추가: 구글 로그인 유저를 위한 기본정보 입력창 activity 추가

---
+++ app/src/main/java/com/example/suitupdaily/UserBasicInfo.java
@@ -0,0 +1,14 @@
+package com.example.suitupdaily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserBasicInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_basic_info);
+    }
+}
