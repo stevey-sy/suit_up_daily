421b2f43163f68a77103e8709aec0702c3d8910e
stevey-sy <sinsy89@hanmail.net>
2020-12-28 15:15:16 +0900

    추가: 코디 게시글 read 페이지 추가

---
+++ app/src/main/java/com/example/suitupdaily/CodiArticle.java
@@ -0,0 +1,14 @@
+package com.example.suitupdaily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CodiArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_codi_article);
+    }
+}
