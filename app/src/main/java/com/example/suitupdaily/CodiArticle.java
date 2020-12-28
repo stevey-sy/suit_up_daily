8e44bf0fd9916079f86395c5ad3b95603903989f
stevey-sy <sinsy89@hanmail.net>
2020-12-28 17:46:10 +0900

    수정: 코디 게시글 / 좋아요 데이터 가져오기 성공, 좋아요 버튼 작동 확인

--- app/src/main/java/com/example/suitupdaily/CodiArticle.java
+++ app/src/main/java/com/example/suitupdaily/CodiArticle.java
@@ -8,6 +8,9 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,14 +22,15 @@ import org.w3c.dom.Text;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class CodiArticle extends AppCompatActivity {
+public class CodiArticle extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     private Toolbar toolbar;
     private ActionBar actionBar;
-    private String user_id, date, like_num, view_num, memo, hash_tags, profile_url, writer_nick, codi_url;
+    private String user_id, date, like_num, view_num, memo, hash_tags, profile_url, writer_nick, codi_url, who_liked;
     private TextView text_writer_name, text_date, text_view_num, text_like_num, text_reply_num, text_hash_tags, text_memo;
     private ImageView img_codi;
     private CircleImageView writer_pic;
+    private CheckBox check_like;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,8 @@ public class CodiArticle extends AppCompatActivity {
         memo = intent_id.getStringExtra("memo");
         codi_url = intent_id.getStringExtra("picture");
         profile_url = intent_id.getStringExtra("profile_pic");
+        who_liked = intent_id.getStringExtra("who_liked");
+        like_num = intent_id.getStringExtra("like_num");
 
         // 툴바 세팅
         toolbar = findViewById(R.id.toolbar_article);
@@ -61,8 +67,11 @@ public class CodiArticle extends AppCompatActivity {
         text_reply_num = (TextView) findViewById(R.id.tv_article_reply_num);
         writer_pic = (CircleImageView)findViewById(R.id.civ_article_profile);
         img_codi = (ImageView)findViewById(R.id.iv_article_codi);
+        check_like = (CheckBox)findViewById(R.id.cb_article_like);
         // intent 로 부터 받아온 데이터를 view 에 뿌려준다.
         setDataFromIntent();
+        // 좋아요 버튼 클릭 리스너 생성
+        check_like.setOnCheckedChangeListener(this);
     }
 
     // 인텐트로 받아온 데이터를 뷰에 뿌려주는 메소드
@@ -91,6 +100,19 @@ public class CodiArticle extends AppCompatActivity {
                 .load(codi_url)
                 .apply(requestOptions)
                 .into(img_codi);
+        // 좋아요 개수 가져오기 && 사용자가 좋아요 눌렀던 글인지 확인하기
+        if(who_liked.contains(user_id)) {
+            // 리스트에 이미 id가 존재한다면 좋아요 누른처리
+//            int like = Integer.parseInt(like_num);
+//            int like_edit = like -1;
+//            text_like_num.setText(String.valueOf(like_edit));
+            text_like_num.setText(like_num);
+            check_like.setChecked(true);
+        } else {
+            // 리스트에 id가 존재하지 않는다면 좋아요 버튼 default 처리
+            check_like.setChecked(false);
+            text_like_num.setText(like_num);
+        }
     }
 
     // 툴바 메뉴 클릭시 이벤트 (홈 버튼)
@@ -102,5 +124,22 @@ public class CodiArticle extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    // 좋아요 체크박스 클릭 이벤트트
+   @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        // 좋아요 버튼 눌렸을 때
+        if (isChecked) {
+            String like_string = text_like_num.getText().toString();
+            int like_int = Integer.parseInt(like_string);
+            like_int += 1;
+            text_like_num.setText(String.valueOf(like_int));
+        } else {
+            // 좋아요 버튼 한번 더 눌렀을 때
+            String like_string = text_like_num.getText().toString();
+            int like_int = Integer.parseInt(like_string);
+            like_int -= 1;
+            text_like_num.setText(String.valueOf(like_int));
+        }
     }
 }
